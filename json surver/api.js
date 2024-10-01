bef6b82d7f79ce5dbea1ac024792960fe8066ab1
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-10-01 14:32:00 +0530

    API

---
+++ json surver/api.js
@@ -0,0 +1,28 @@
+const studentapi={
+    post:async(student)=>{
+        let req=await fetch("http://localhost:3000/students",{
+            method:"POST",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(student)
+        })
+    },
+    get:async()=>{
+        let req=await fetch("http://localhost:3000/students");
+        let res=await req.json();
+        return res
+    },
+    patch:async(id,student)=>{
+        let req=await fetch(`http://localhost:3000/students/${id}`,{
+            method:"POST",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(student)
+        })
+    },
+    delete:async(id)=>{
+        let req=await fetch(`http://localhost:3000/students/${id}`,{
+            method:"DELETE",
+        })
+    }
+}
+
+export default studentapi
---
+++ json surver/index.js
@@ -0,0 +1,57 @@
+import studentapi from "./api.js";
+
+let id=-1
+const handlesubmit=(e)=>{
+    e.preventDefault();
+    let student={
+        name:document.getElementById("name").value,
+        number:document.getElementById("number").value,
+        course:document.getElementById("course").value
+    }
+    console.log(student);
+    if(id==-1){
+        studentapi.post(student)
+    }
+    else{
+        studentapi.patch(id,student)
+    }
+    window.location.reload()
+}
+document.getElementById("form").addEventListener("submit",handlesubmit)
+
+const handleupdate=(ele)=>{
+    document.getElementById("name").value=ele.name;
+    document.getElementById("number").value=ele.number;
+    document.getElementById("course").value=ele.course;
+    document.getElementById("type").value="update";
+    id=ele.id;
+}
+
+const uimaker=(student)=>{
+    student.map((ele)=>{
+        let div=document.createElement("div");
+        let name=document.createElement("h1");
+        name.innerHTML=ele.name;
+        let course=document.createElement("h2");
+        course.innerHTML=ele.course;
+        let number=document.createElement("h3");
+        number.innerHTML=ele.number;
+        let btn1=document.createElement("button");
+        btn1.innerHTML="Delete";
+        let btn2=document.createElement("button");
+        btn2.innerHTML="Update";
+        btn2.addEventListener("click",()=>handleupdate(ele))
+        btn1.addEventListener("click",()=>{
+            studentapi.delete(ele.id)
+            window.location.reload()
+        })
+        div.append(name,course,number,btn1,btn2)
+        document.getElementById("list").append(div)
+    })
+}
+
+const getstudent=async()=>{
+    console.log(await studentapi.get());
+    uimaker(await studentapi.get())
+}
+getstudent()
